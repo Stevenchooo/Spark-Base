ecd1b4690c65d88fcb27a61334c4c6c5add95aa7
Steven <cszcszohye@qq.com>
2017-08-14 16:44:11 +0800

    RDD transformation operations

--- src/main/scala/es/testRDD.scala
+++ src/main/scala/es/testRDD.scala
@@ -10,31 +10,25 @@ object testRDD {
   def main(args: Array[String]): Unit = {
     val sc = new SparkContext(new SparkConf().setAppName("Steven005").setMaster("local"))
 
-    //-----------------------------------------------------------------------------------
-    val rdd03 = sc.parallelize(list, 1)
-    val r03 = rdd03.map { x => x + 1 }
-    println(r03.collect().mkString(","))
+    /*
+       map()           参数是函数，函数应用于RDD每一个元素，返回值是新的RDD
+      flatMap()       参数是函数，函数应用于RDD每一个元素，将元素数据进行拆分，变成迭代器，返回值是新的RDD
+      filter()        参数是函数，函数会过滤掉不符合条件的元素，返回值是新的RDD
+      distinct()      没有参数，将RDD里的元素进行去重操作
+      union()         参数是RDD，生成包含两个RDD所有元素的新RDD
+      intersection()  参数是RDD，求出两个RDD的共同元素
+      subtract()      参数是RDD，将原RDD里和参数RDD里相同的元素去掉
+      cartesian()     参数是RDD，求两个RDD的笛卡儿积
 
-    val r04 = rdd03.filter { x => x > 3 }
-    println(r04.collect().mkString(","))
+      collect()                     返回RDD所有元素
+      count()                       RDD里元素个数
+      countByValue()                各元素在RDD中出现次数
+      reduce()                      并行整合所有RDD数据，例如求和操作
+      fold(0)(func)                 和reduce功能一样，不过fold带有初始值
+      aggregate(0)(seqOp,combop)    和reduce功能一样，但是返回的RDD数据类型和原RDD不一样
+      foreach(func)                 对RDD每个元素都是使用特定函数
 
-    //新建Tuple
-    val pair = (99, "Luftballons", sc.getConf.toString)
-    println(pair._3)
-
-    val rdd = sc.textFile("1.txt", 1);
-    val r = rdd.flatMap { x => x.split(" ") }
-    println(r.collect().mkString(","))
-    //-----------------------------------------------------------------------------------
-    /*Transformation &
-    去重distinct()，合并union()，共同元素intersection()，去掉两个RDD相同的元素subtract()，
-    笛卡尔积cartesian()
     */
-    /* Action
-    返回所有元素collect()，个数count()，各元素出现次数countByValue()，并行整合所有元素reduce()。
-    fold(0)(func)，aggregate(0)(seqOp,combop)，对每个元素使用特定函数foreach(func)
-    */
-
     val rddInt: RDD[Int] = sc.makeRDD(List(1, 2, 3, 4, 5, 6, 2, 5, 1))
     val rddStr: RDD[String] = sc.parallelize(Array("a", "b", "c", "d", "b", "a"), 1)
     val rddFile: RDD[String] = sc.textFile("1.txt", 1)
@@ -47,7 +41,41 @@ object testRDD {
     println(rddInt.map(x => x + 1).collect().mkString(","))
     println("======map操作======")
 
+    /* filter操作 */
+    println("======filter操作======")
+    println(rddInt.filter(x => x > 4).collect().mkString(","))
+    println("======filter操作======")
 
+    /* flatMap操作 */
+    println("======flatMap操作======")
+    println(rddFile.flatMap { x => x.split(",") }.first())
+    println("======flatMap操作======")
+
+    /* distinct去重操作 */
+    println("======distinct去重======")
+    println(rddInt.distinct().collect().mkString(","))
+    println(rddStr.distinct().collect().mkString(","))
+    println("======distinct去重======")
+
+    /* union操作 */
+    println("======union操作======")
+    println(rdd01.union(rdd02).collect().mkString(","))
+    println("======union操作======")
+
+    /* intersection操作 */
+    println("======intersection操作======")
+    println(rdd01.intersection(rdd02).collect().mkString(","))
+    println("======intersection操作======")
+
+    /* subtract操作 */
+    println("======subtract操作======")
+    println(rdd01.subtract(rdd02).collect().mkString(","))
+    println("======subtract操作======")
+
+    /* cartesian操作 */
+    println("======cartesian操作======")
+    println(rdd01.cartesian(rdd02).collect().mkString(","))
+    println("======cartesian操作======")
   }
 
 }
